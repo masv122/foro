8bf9445fee56f4b4da95efd44de57588e2e431e8
Marcel <masv122@gmail.com>
2020-05-02 12:52:31 -0400

    cambio barra superior

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,29 @@
 <template>
   <div id="app">
-    <div id="nav"></div>
-    <b-container fluid id="pagina">
-      <b-row>
-        <b-col class="ColBarraLateral bg-primary shadow" cols="1">
+    <!-- <b-col class="ColBarraLateral bg-primary shadow" cols="1">
           <BarraLateral class="BarraLateral"/>
-        </b-col>
-        <b-col class="contenido pb-5" cols="11">
-          <BarraSuperior />
-          <b-container fluid>
-            <router-view />
-          </b-container>
-        </b-col>
-      </b-row>
+        </b-col> -->
+    <BarraSuperior />
+    <b-container fluid class="contenido">
+      <router-view />
     </b-container>
   </div>
 </template>
 
 <script>
 import BarraSuperior from "@/components/BarraSuperior.vue";
-import BarraLateral from "@/components/BarraLateral.vue";
+// import BarraLateral from "@/components/BarraLateral.vue";
 export default {
   name: "App",
   components: {
     BarraSuperior,
-    BarraLateral
+    // BarraLateral
   }
 };
 </script>
 
 <style scoped>
-  .ColBarraLateral{
-    padding-top: 5%;
-    padding-bottom: 10%;
+  .contenido{
+    margin-top: 5rem;
   }
- 
 </style>
--- src/components/BarraSuperior.vue
+++ src/components/BarraSuperior.vue
@@ -1,64 +1,64 @@
 <template>
   <div>
-    <b-navbar class="my-3" toggleable="lg" type="primary" variant="white">
-      <b-navbar-brand :to="{ name: 'Home' }"
-        ><i class="fa fa-home" aria-hidden="true"></i
-      ></b-navbar-brand>
-      <b-collapse id="navbar-toggle-collapse" is-nav>
-        <b-navbar-nav class="ml-auto">
-          <b-nav-form>
-            <b-form-group>
-              <b-input-group class="inputGroup border-primary">
-                <template v-slot:prepend>
-                  <b-input-group-text class="bg-white slot border-primary"
-                    ><i class="fa fa-search" aria-hidden="true"></i
-                  ></b-input-group-text>
-                </template>
-                <b-form-input
-                  id="input-1"
-                  type="text"
-                  required
-                  placeholder="Buscar"
-                  class="input border-primary"
-                >
-                </b-form-input>
-              </b-input-group>
-            </b-form-group>
-          </b-nav-form>
-          <b-navbar-nav v-if="!loginVisible" class="usuario">
-            <b-nav-item href="#"
-              ><i class="fa fa-user-circle"
-                ><sup>
-                  <i class="fa fa-medal"
-                    ><sup><b-badge pill variant="light">0</b-badge></sup></i
-                  >
-                  <i class="fa fa-medal"
-                    ><sup><b-badge pill variant="light">0</b-badge></sup></i
-                  >
-                  <i class="fa fa-medal"
-                    ><sup><b-badge pill variant="light">0</b-badge></sup></i
-                  >
-                </sup>
-              </i>
-            </b-nav-item>
-            <b-nav-item
-              ><i class="fa fa-bell" aria-hidden="true"
-                ><sup><b-badge pill variant="light">0</b-badge></sup></i
-              ></b-nav-item
-            >
-          </b-navbar-nav>
-          <b-nav-item-dropdown no-caret right v-if="!loginVisible">
-            <template v-slot:button-content>
-              <i class="fa fa-cogs" aria-hidden="true"></i>
-            </template>
-            <b-dropdown-item href="#">Profile</b-dropdown-item>
-            <b-dropdown-item :to="{ name: 'Login' }">Sign Out</b-dropdown-item>
-          </b-nav-item-dropdown>
-          <b-nav-item v-if="loginVisible">
-            <i class="fa fa-user-circle" aria-hidden="true"></i>
-          </b-nav-item>
-        </b-navbar-nav>
-      </b-collapse>
+    <b-navbar
+      class="shadow-sm fixed-top"
+      type="primary"
+      variant="light"
+    >
+      <!-- <b-navbar-brand ></b-navbar-brand> -->
+      <b-navbar-nav class="mr-auto nav-left">
+        <b-nav-form>
+          <b-form-group>
+            <b-input-group class="inputGroup border-primary">
+              <template v-slot:prepend>
+                <b-input-group-text class="bg-white slot border-primary"
+                  ><i class="fa fa-search" aria-hidden="true"></i
+                ></b-input-group-text>
+              </template>
+              <b-form-input
+                id="input-1"
+                type="text"
+                required
+                placeholder="Buscar"
+                class="input border-primary"
+              >
+              </b-form-input>
+            </b-input-group>
+          </b-form-group> </b-nav-form
+      ></b-navbar-nav>
+      <b-navbar-nav class="mx-auto nav-center">
+        <b-nav-item :to="{ name: 'Home' }">
+          <i class="fa fa-home" aria-hidden="true"></i>
+        </b-nav-item>
+        <b-nav-item><i class="fa fa-fire" aria-hidden="true"></i></b-nav-item>
+        <b-nav-item><i class="fa fa-star" aria-hidden="true"></i></b-nav-item>
+        <b-nav-item><i class="fa fa-list" aria-hidden="true"></i></b-nav-item>
+        <b-nav-item><i class="fa fa-heart" aria-hidden="true"></i></b-nav-item>
+      </b-navbar-nav>
+      <b-navbar-nav class="ml-auto nav-right">
+        <b-nav-item href="#"><i class="fa fa-user-circle"> </i> </b-nav-item>
+        <b-nav-item
+          ><i class="fa fa-bell" aria-hidden="true"
+            ><sup><b-badge pill variant="light">0</b-badge></sup></i
+          ></b-nav-item
+        >
+        <b-nav-item
+          ><i class="fa fa-envelope-open" aria-hidden="true"></i
+        ></b-nav-item>
+        <b-nav-item-dropdown no-caret right v-if="!loginVisible">
+          <template v-slot:button-content>
+            <i class="fa fa-cogs" aria-hidden="true"></i>
+          </template>
+          <b-dropdown-item href="#">Profile</b-dropdown-item>
+          <b-dropdown-item :to="{ name: 'Login' }">Sign Out</b-dropdown-item>
+        </b-nav-item-dropdown>
+        <b-nav-item v-if="loginVisible">
+          <i class="fa fa-user-circle" aria-hidden="true"></i>
+        </b-nav-item>
+        <b-nav-item>
+          <i class="fa fa-question-circle" aria-hidden="true"></i
+        ></b-nav-item>
+      </b-navbar-nav>
       <b-navbar-toggle target="navbar-toggle-collapse">
         <template v-slot:default="{ expanded }">
           <i v-if="expanded" class="fa fa-chevron-up" aria-hidden="true"></i>
@@ -80,14 +80,12 @@ export default {
 </script>
 
 <style  scoped>
-.fa-medal {
-  font-size: 1rem;
-}
-.fa-user-circle, .usuario .fa-user-circle,
-.fa-cogs,
-.fa-home,
-.usuario .fa-bell {
+.nav-center i {
   font-size: 2rem;
+  margin: 0 2rem;
+}
+.nav-right i {
+  font-size: 1.5rem;
 }
 .inputGroup {
   color: #495057;
